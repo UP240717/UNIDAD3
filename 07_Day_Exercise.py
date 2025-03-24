784fde044985e2b01c25d19c05f24d867cde340b
UP240717 <up240717@alumnos.upa.edu.mx>
2025-03-24 15:11:22 -0600

    modified:   07_Day_Exercise.py 	new file:   09_Day_Exercises.py

--- 07_Day_Exercise.py
+++ 07_Day_Exercise.py
@@ -1,22 +1,30 @@
-# sets
+#Level 1
+
+#1
 it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
 
 print(f"The lenght of it companies is {len(it_companies)}")
 
+#2
 it_companies.add("Twitter")
 print(it_companies)
 
+#3
 it_companies.update(["Youtube","Linux","Opera"])
 print(it_companies)
 
+#4
 it_companies.discard("Youtube")
 print(it_companies)
 
+#5
 print("The discard() method removes the specified element from the set. Unlike the remove() method, discard() does not raise an error if the specified element is not present.")
 
+#Level 2
 
 A = {19, 22, 24, 20, 25, 26}
 B = {19, 22, 20, 25, 26, 24, 28, 27}
+
 
 C = A.union(B)
 print(C)
@@ -32,6 +40,8 @@ print(A.symmetric_difference(B))
 
 del A,B
 
+#Level 3
+
 age_list = [22, 19, 24, 25, 26, 24, 25, 24]
 
 age_set = set(age_list)
@@ -44,13 +54,6 @@ elif len(age_set) > len(age_list):
 
 else:
     print('List is bigger')
-
-'''
-String: Text-based data.
-List: Ordered, mutable collection of items.
-Tuple: Ordered, immutable collection of items.
-Set: Unordered collection of unique items.
-'''
 
 sentence = 'I am a teacher and I love to inspire and teach people'
 words = set(sentence.split())
